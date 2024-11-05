3174f4e0c26c35800c5ad8c0ef939f06462015a4
Diana Dutko <dutkodiana2007@gmail.com>
2024-11-05 22:53:55 +0200

    4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+const btn = document.querySelector('button[type="submit"]');
+const input = document.querySelector('input');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formElements = form.elements;
+
+    for (const element of formElements) {
+        if (element.tagName === 'INPUT' && element.value.trim() === '') {
+            alert('All form fields must be filled in');
+            return;
+        }
+    }
+
+    const dataObj = {
+        email: formElements.email.value.trim(),
+        password: formElements.password.value.trim(),
+    }
+
+    console.log(dataObj);
+    
+    form.reset();
+})
