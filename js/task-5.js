a5fd1d3f1c6874e2100f6e36fdf7d690cc202b89
Diana Dutko <dutkodiana2007@gmail.com>
2024-11-05 23:05:04 +0200

    5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const output = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  output.textContent = getRandomHexColor();
+})
